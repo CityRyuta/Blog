d745e9566d53bbf2a68e2cfd1d087c93971a4f16
CityRyuta <s2019026@surugadai.ac.jp>
2022-04-24 20:00:56 +0900

    [add]投稿の削除

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,19 +3,51 @@
 namespace App\Http\Controllers;
 
 use App\Post;
-use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest; // useする
 
 class PostController extends Controller
 {
-        /**
-     * Post一覧を表示する
-     * 
-     * @param Post Postモデル
-     * @return array Postモデルリスト
-     */
     public function index(Post $post)
     {
         return view('posts/index')->with(['posts' => $post->getPaginateByLimit()]);
     }
+
+    public function show(Post $post)
+    {
+        return view('posts/show')->with(['post' => $post]);
+    }
+
+    public function create()
+    {
+        return view('posts/create');
+    }
+
+    public function store(PostRequest $request, Post $post) // 引数をRequest->PostRequestにする
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
+    }
+    
+    /*ブログ投稿編集画面表示用*/
+    public function edit(Post $post)
+    {
+        return view('posts/edit')->with(['post' => $post]);
+    }
+    
+    
+    /*ブログ投稿編集実行用*/
+    public function update(PostRequest $request, Post $post)
+    {
+        $input_post = $request['post'];
+        $post->fill($input_post)->save();
+    
+        return redirect('/posts/' . $post->id);
+    }
+    
+    public function delete(Post $post)
+    {
+        $post->delete();
+        return redirect('/');
+    }
 }
-?>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -13,7 +13,10 @@
         <div class='posts'>
             @foreach ($posts as $post)
                 <div class='post'>
-                    <h2 class='title'>{{ $post->title}}</h2>
+                    <p class='create'>[<a href='/posts/create'>create</a>]</p>
+                    <h2 class='title'>
+                        <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
+                    </h2>
                     <p class='body'>{{ $post->body}}</p>
                 </div>
             @endforeach
